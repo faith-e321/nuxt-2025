4ce44cd604ffa83e517bcd63cc6bad4c053dbac8
faith-e321 <138939265+faith-e321@users.noreply.github.com>
2025-05-04 13:53:52 -0700

    Update footer

--- components/Layouts/Footer.vue
+++ components/Layouts/Footer.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="bg-gray-800 py-14">
-        <footer class="text-white px-4 max-w-5xl mx-auto flex justify-between items-center">
-            <div>
-                <img src="/faithlogo.png" alt="logo" class="h-12 p-2 mr-2">
+        <footer class="text-white px-4 max-w-5xl mx-auto flex flex-col md:flex-row justify-between items-start gap-4">
+            <div class="flex-shrink-0">
+                <img src="/faithlogo.png" alt="logo" class="h-10 pb-2">
             </div>
-            <div>
-                <NavigationFooter />
+            <div class="w-full md:w-auto">
+                <NavigationFooter class="flex flex-col md:flex-row items-start gap-4"/>
             </div>
         </footer>
     </div>
